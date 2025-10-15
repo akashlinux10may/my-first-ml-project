12adda32481a4b00f060e745a7b8f3d897006023
Akash shrivastava <akash.10may@gmail.com>
2025-10-15 13:38:03 +0530

    Add data normlaization & .gitignore

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def validate_input(data):
     if len(data.shape) != 2:
         raise ValueError("Data must be 2D")
     return True
+
+def normalize_data(data):
+    """Normalize data to 0-1 range"""
+    min_val = data.min()
+    max_val = data.max()
+    return (data - min_val) / (max_val - min_val)
