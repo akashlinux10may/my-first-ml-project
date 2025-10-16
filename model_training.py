ed9fc766754f67bb254e3541bac8760b5f483f60
Akash shrivastava <akash.10may@gmail.com>
2025-10-16 15:04:42 +0530

    Increase n_estimators to 100

--- model_training.py
+++ model_training.py
@@ -8,7 +8,7 @@ def train_model():
     X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     y = np.array([0, 1, 0])
     
-    model = RandomForestClassifier(n_estimators=10, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X, y)
     
     # Save model
